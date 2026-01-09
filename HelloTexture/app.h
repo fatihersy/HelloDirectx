eb52a28b88816112b90285fc273a1c19a052b314
fatihersy <fatih.r.ersoy@gmail.com>
2026-01-09 23:00:10 +0300

    HelloTexture

---
+++ HelloTexture/app.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <vector>
+
+#include "IApp.h"
+
+using namespace DirectX;
+
+// Note that while ComPtr is used to manage the lifetime of resources on the CPU,
+// it has no understanding of the lifetime of resources on the GPU. Apps must account
+// for the GPU lifetime of resources to avoid destroying objects that may still be
+// referenced by the GPU.
+// An example of this can be found in the class method: OnDestroy().
+using Microsoft::WRL::ComPtr;
+
+class app : public IApp
+{
+public:
+	app(UINT width, UINT height, std::wstring name, HINSTANCE hInstance, int nCmdShow);
+	~app();
+
+	void Run();
+
+	void OnInit() override;
+	void OnUpdate() override;
+	void OnRender() override;
+	void OnDestroy() override;
+
+	void OnKeyDown(UINT8 key) override;
+	void OnKeyUp(UINT8 key) override;
+
+private:
+	static const UINT FrameCount = 2;
+	static const UINT TextureWidth = 256;
+	static const UINT TextureHeight = 256;
+	static const UINT TexturePixelSize = 4; // The number of bytes to represent a pixel in the texture
+
+	struct Vertex
+	{
+		XMFLOAT3 position;
+		XMFLOAT2 uv;
+	};
+
+	// Pipeline objects.
+	CD3DX12_VIEWPORT m_viewport;
+	CD3DX12_RECT m_scissorRect;
+	ComPtr<IDXGISwapChain4> m_swapChain;
+	ComPtr<ID3D12Device> m_device;
+	ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+	ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+	ComPtr<ID3D12CommandQueue> m_commandQueue;
+	ComPtr<ID3D12RootSignature> m_rootSignature;
+	ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+	ComPtr<ID3D12DescriptorHeap> m_srvHeap;
+	ComPtr<ID3D12PipelineState> m_pipelineState;
+	ComPtr<ID3D12GraphicsCommandList> m_commandList;
+	UINT m_rtvDescriptorSize;
+
+	// App resources.
+	ComPtr<ID3D12Resource> m_vertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+	ComPtr<ID3D12Resource> m_texture;
+
+	// Synchronization objects.
+	UINT m_frameIndex;
+	HANDLE m_fenceEvent;
+	ComPtr<ID3D12Fence> m_fence;
+	UINT64 m_fenceValue;
+
+	void LoadPipeline();
+	void LoadAssets();
+	std::vector<UINT8> GenerateTextureData();
+	void PopulateCommandList();
+	void WaitForPreviousFrame();
+
+	inline std::wstring GetAssetFullPath(LPCWSTR assetName) {
+		return m_assetsPath + assetName;
+	}
+
+	UINT m_width;
+	UINT m_height;
+	std::wstring m_title;
+	float m_aspectRatio;
+
+	// Root assets path.
+	std::wstring m_assetsPath;
+};
+
